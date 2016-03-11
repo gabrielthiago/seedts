9883d104efa219daef96d0b25348f06802f03836
gabrielthiago <gabrielthiago@hotmail.com>
2016-03-11 11:51:04 -0300

    Páginas de Serviços e Empresa
    Adição de diversas páginas em serviços e empresas.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 
-  var $window = $(window); //You forgot this line in the above example
+  var $window = $(window);
 
   $('section[data-type="background"]').each(function(){
     var $bgobj = $(this); // assigning the object
@@ -12,4 +12,31 @@ $(document).ready(function(){
       $bgobj.css({ backgroundPosition: coords });
     });
   });
+
+  var $animation_elements = $('.entrada');
+  var $window = $(window);
+
+  function check_if_in_view() {
+    var window_height = $window.height();
+    var window_top_position = $window.scrollTop();
+    var window_bottom_position = (window_top_position + window_height);
+
+    $.each($animation_elements, function() {
+      var $element = $(this);
+      var element_height = $element.outerHeight();
+      var element_top_position = $element.offset().top;
+      var element_bottom_position = (element_top_position + element_height);
+
+      //check to see if this current container is within viewport
+      if ((element_bottom_position >= window_top_position) &&
+        (element_top_position <= window_bottom_position)) {
+        $element.addClass('fadeIn');
+      }
+    });
+  }
+
+  $window.on('scroll resize', check_if_in_view);
+  $window.trigger('scroll');
+
+
 });
